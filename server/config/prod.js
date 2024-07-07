abc5e5752181162f7b6ad1a326585c526778ee93
Aryan Agrawal <45911109+Aaryan00@users.noreply.github.com>
2024-07-07 14:31:34 +0530

    Update prod.js

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI:"mongodb+srv://Aryan_agrawal:<password>@cluster0.ddo9uut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  mongoURI:"mongodb+srv://Aryan_agrawal:Aryan1234@@cluster0.ddo9uut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 };
